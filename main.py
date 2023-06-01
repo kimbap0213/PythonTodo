291210090d6e202f4ef5eb9e140f961242ca0121
kimbap0213 <35618313+kimbap0213@users.noreply.github.com>
2023-06-01 21:58:49 +0900

    Feat 추가

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-from tkinter import *
-
-tk = Tk()
-stringArr = [];
-label = Label(tk, text = "aabb")
+import tkinter
+import tkinter.ttk
 
 def add():
-    stringArr.append("aa")
+    stringArr.append(entryinput.get())
     label['text'] = stringArr
+    entryinput.delete(0,"end")
 
-label.pack(side = LEFT, padx = 10, pady = 10)
-button = Button(tk, text = "plus" , command = add)
-button.pack(sid = LEFT, padx = 10, pady = 10)
+tk = tkinter.Tk()
+tk.title("PythonTodo")
+stringArr = [];
+label = tkinter.Label(tk, text = "aabb")
+entryinput = tkinter.Entry(tk)
+entryinput.grid(row=0, column = 0)
+label.grid(row=1, column = 0)
+button = tkinter.Button(tk, text = "plus" , command = add)
+button.grid(row = 2, column = 0)
+treeview = tkinter.ttk.Treeview(tk, columns=["할일","날짜"], displaycolumns=["할일","날짜"])
+treeview.grid(row=3,column=0)
 tk.mainloop()
