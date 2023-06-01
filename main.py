5de51c9eb154e5bac0bd27e90ba188857cb65768
kimbap0213 <35618313+kimbap0213@users.noreply.github.com>
2023-06-01 15:37:16 +0900

    Refactor 함수 재정의

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from tkinter import *
 
-def add(arr, string):
-    arr.append(string)
-    label['text'] = arr
-    print(arr)
-
 tk = Tk()
 stringArr = [];
 label = Label(tk, text = "aabb")
+
+def add():
+    stringArr.append("aa")
+    label['text'] = stringArr
+
 label.pack(side = LEFT, padx = 10, pady = 10)
-button = Button(tk, text = "plus" , command = add(stringArr, "a"))
+button = Button(tk, text = "plus" , command = add)
 button.pack(sid = LEFT, padx = 10, pady = 10)
 tk.mainloop()
